71165588564218a709c93fc03398615a7105aa35
Bashmund <bashmundkhan@yahoo.com>
2019-03-29 17:19:04 +0500

    register activity created, forms creation is remaining

--- app/src/main/java/com/example/complaintmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/complaintmanagementsystem/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.complaintmanagementsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
