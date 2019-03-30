de2f09629cc405afac52dc7f8a8c24df184a496c
Bashmund <bashmundkhan@yahoo.com>
2019-03-30 18:31:36 +0500

    FULLY UPDATED MASTER

--- app/src/main/java/com/example/complaintmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/complaintmanagementsystem/MainActivity.java
@@ -12,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logIn=(Button)findViewById(R.id.logIn);
+        Button signUp=(Button)findViewById(R.id.signUp);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),Signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
