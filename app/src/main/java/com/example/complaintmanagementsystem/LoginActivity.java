fadd12bd8d1d0e7a2caeb7cb8188b097b891b3ad
Bashmund <bashmundkhan@yahoo.com>
2019-03-31 15:31:30 +0500

    work in progress

--- app/src/main/java/com/example/complaintmanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/complaintmanagementsystem/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.complaintmanagementsystem;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
@@ -8,9 +9,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,23 +24,24 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout textUserID;
     private TextInputLayout textPassword;
     private Button logIn;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         textUserID=findViewById(R.id.text_userID);
         textPassword=findViewById(R.id.text_password);
         logIn=findViewById(R.id.LLogin);
+        progressDialog= new ProgressDialog(this);
+
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!validateUserID() | !validatePassword()){
                     return;
                 }
-                String input="UserID: "+textUserID.getEditText().getText().toString();
-                input+="\n";
-                input +="Password: "+textPassword.getEditText().getText().toString();
-                Toast.makeText(LoginActivity.this, input, Toast.LENGTH_SHORT).show();
+                registerUser();
             }
         });
     }
@@ -63,5 +68,12 @@ public class LoginActivity extends AppCompatActivity {
             textPassword.setError(null);
             return true;
         }
+    }
+    private void registerUser(){
+        String userID=textUserID.getEditText().getText().toString();
+        String password=textPassword.getEditText().getText().toString();
+
+        progressDialog.setMessage("Registering User...\n");
+        progressDialog.show();
     }
 }
