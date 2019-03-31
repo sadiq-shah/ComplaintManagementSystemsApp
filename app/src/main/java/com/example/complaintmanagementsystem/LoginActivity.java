94c67350af48f97a6995ee0dad9157c42a8fdabb
Bashmund <bashmundkhan@yahoo.com>
2019-03-31 16:40:16 +0500

    Login authentication working

--- app/src/main/java/com/example/complaintmanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/complaintmanagementsystem/LoginActivity.java
@@ -1,39 +1,36 @@
 package com.example.complaintmanagementsystem;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextInputLayout textUserID;
+    private TextInputLayout textEmailAddress;
     private TextInputLayout textPassword;
     private Button logIn;
-    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        textUserID=findViewById(R.id.text_userID);
+        textEmailAddress=findViewById(R.id.text_emailAddress);
         textPassword=findViewById(R.id.text_password);
         logIn=findViewById(R.id.LLogin);
-        progressDialog= new ProgressDialog(this);
+        firebaseAuth=FirebaseAuth.getInstance();
 
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,18 +39,23 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
                 registerUser();
+
             }
         });
     }
     private boolean validateUserID(){
-        String uID=textUserID.getEditText().getText().toString().trim();
-        if(uID.isEmpty())
+        String emailAddress=textEmailAddress.getEditText().getText().toString().trim();
+        if(emailAddress.isEmpty())
         {
-            textUserID.setError("Field cannot be empty");
+            textEmailAddress.setError("Field cannot be empty");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()){
+            textEmailAddress.setError("Please enter a valid email address");
             return false;
         }
         else{
-            textUserID.setError(null);
+            textEmailAddress.setError(null);
             return true;
         }
     }
@@ -69,11 +71,22 @@ public class LoginActivity extends AppCompatActivity {
             return true;
         }
     }
-    private void registerUser(){
-        String userID=textUserID.getEditText().getText().toString();
+    private void registerUser()
+    {
+        String userID=textEmailAddress.getEditText().getText().toString().trim();
         String password=textPassword.getEditText().getText().toString();
 
-        progressDialog.setMessage("Registering User...\n");
-        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(userID,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "User logged in", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "User not logged in", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/complaintmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/complaintmanagementsystem/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        FirebaseApp.initializeApp(getBaseContext());
         Button logIn=(Button)findViewById(R.id.logIn);
         Button signUp=(Button)findViewById(R.id.signUp);
 
