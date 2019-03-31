7622d72d9727e8c6c3c622406d22cc899eb8b750
Bashmund <bashmundkhan@yahoo.com>
2019-03-31 14:41:32 +0500

    gradle updated for firebase

--- app/src/main/java/com/example/complaintmanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/complaintmanagementsystem/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.example.complaintmanagementsystem;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         textUserID=findViewById(R.id.text_userID);
         textPassword=findViewById(R.id.text_password);
         logIn=findViewById(R.id.LLogin);
-
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
