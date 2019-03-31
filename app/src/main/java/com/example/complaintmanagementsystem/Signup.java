0d58ed3a70e5dbd83c1eab8ae0e72e85b0f3a88f
Yushi Bazukay <sadiq484473@gmail.com>
2019-03-31 09:58:42 +0500

    User Registration

--- app/src/main/java/com/example/complaintmanagementsystem/Signup.java
+++ app/src/main/java/com/example/complaintmanagementsystem/Signup.java
@@ -1,7 +1,9 @@
 package com.example.complaintmanagementsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void StudentRegistration(View v)
+    {
+        Intent intent = new Intent(this, UserRegistration.class);
+        startActivity(intent);
     }
 }
