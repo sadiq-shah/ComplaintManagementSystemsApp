452c4d8703829ed14e8e9981fd425db43c473d10
Bashmund <bashmundkhan@yahoo.com>
2019-04-06 13:44:02 +0500

    Fragments implemented only,working remaining

---
+++ app/src/main/java/com/example/complaintmanagementsystem/ProfileSettingsFragment.java
@@ -0,0 +1,29 @@
+package com.example.complaintmanagementsystem;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileSettingsFragment extends Fragment {
+
+
+    public ProfileSettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_settings, container, false);
+    }
+
+}
